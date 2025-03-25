695ad2f5b8d16a929f4d36897160d35930573cdf
Mark Kravchuk <markkravchuk123@icloud.com>
2025-03-25 13:52:59 -0700

    Basic server set up

--- server/cmd/main.go
+++ server/cmd/main.go
@@ -1,5 +1,26 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"net/http"
+	"log"
+)
 
+func homePage(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "home page")
+}
+
+func wsEndpoint(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "websocket endpoint")
+}
+
+func setupRoutes() {
+	http.HandleFunc("/", homePage)
+	http.HandleFunc("/ws", wsEndpoint)
+}
+
+func main() {
+	fmt.Println("Go websockets")
+	setupRoutes()
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
